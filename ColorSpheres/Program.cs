468a6e4fe8ab0c4c5328175e3c688787a233a00b
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-04-11 22:27:48 +0100

    Added one more example and how it prints the results

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -20,47 +20,58 @@ namespace ColorSpheres
             //Instance of a blue sphere
             Sphere sphere_blue = new Sphere(blue, 6f);
 
-            //Instance of the color blue
-            Color red = new Color(255, 0, 0, 60); // Alpha is set to 255 by default
-            //Instance of a blue sphere
+            //Instance of the color red
+            Color red = new Color(255, 0, 0, 60);
+            //Instance of a red sphere
             Sphere sphere_red = new Sphere(red, 18f);
+
+            //Instance of color1
+            Color color1 = new Color(100, 56, 84, 10);
+            //Instance of a color1 sphere
+            Sphere sphere_color1 = new Sphere(color1, 25f);
 
             //Thrown of the spheres
             sphere_green.Throw(); //1
             sphere_blue.Throw(); //1
             sphere_red.Throw(); //1
+            sphere_color1.Throw(); //1
 
-            sphere_red.Pop(); //Pops the red sphere
+            sphere_green.Pop(); //Pops the red sphere
             sphere_blue.Pop(); //Pops the green sphere
 
-            //Throwing the green sphere five times
+            //Throwing the green and color 1 spheres five more times
             sphere_green.Throw(); //2
             sphere_green.Throw(); //3
             sphere_green.Throw(); //4
             sphere_green.Throw(); //5
             sphere_green.Throw(); //6
 
+            sphere_color1.Throw(); //2
+            sphere_color1.Throw(); //3
+            sphere_color1.Throw(); //4
+            sphere_color1.Throw(); //5
+            sphere_color1.Throw(); //6
+
             //Throwing the blue and red two more times
             sphere_blue.Throw();
             sphere_red.Throw();
 
-            // Print times thrown
-            Console.WriteLine(sphere_green.GetTimesThrown());
-            Console.WriteLine(sphere_blue.GetTimesThrown());
-            Console.WriteLine(sphere_red.GetTimesThrown());
+            //Print times thrown
+            Console.WriteLine($"Green sphere:{sphere_green.GetTimesThrown()}");
+            Console.WriteLine($"Blue sphere:{sphere_blue.GetTimesThrown()}");
+            Console.WriteLine($"Red sphere:{sphere_red.GetTimesThrown()}");
+            Console.WriteLine($"Color1 sphere:{sphere_color1.GetTimesThrown()}");
 
             //No more actions should be done by the popped spheres do to Pop()
             //making it unusable
             
 
             //Results should shown:
-            //Sphere was popped ---> Blue sphere was popped
-            //Sphere was popped ---> Red sphere was popped
-            //6 ---> Number of times the green sphere was thrown
-            //1 ---> Number of times the blue sphere was thrown
-            //1 ---> Number of times the red sphere was thrown
-          
-            //The number of other spheres isn't shown due to them being popped
+            //Green sphere:1
+            //Blue sphere:1
+            //Red sphere:2
+            //Color1 sphere:6
         }
     }
 }
+
